4bc1cdaf92dfb522aaba48daa7f0cea14f8bcb62
IsraelMerlyn <merlyn142a@gmail.com>
2025-05-09 07:51:11 -0600

    Tasks #0.1 Levantamiento del proyecto de Pizza

---
+++ src/main/java/com/israelmerlyn/Api_Pizzeria/ApiPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.israelmerlyn.Api_Pizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPizzeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/israelmerlyn/Api_Pizzeria/ApiPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israelmerlyn.Api_Pizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
